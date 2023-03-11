15bf0aaff02094dfb21a1efe08ef6211fab2f036
mlrt <lakshmi.ravi3003@gmail.com>
2023-03-11 06:48:41 +0530

    application file

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask 
+
+app  = Flask(__name__)
+
+@app.route("/")  # localhost:port_number/home
+
+
+
+def home():
+    return "this is our first flask application"
+
+
+if __name__ =="__main__":
+   app.run(debug=True) 
+
